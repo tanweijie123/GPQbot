817c590493924be4fa5c893108325cfab059bb9d
Tan Wei Jie <tanweijie123@gmail.com>
2021-03-20 12:25:29 +0800

    Fix bug for close confirmation
    Fix bug for close confirmation when non-mod can delete the message

--- GPQbot/src/main/java/App.java
+++ GPQbot/src/main/java/App.java
@@ -17,7 +17,6 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public class App {
--- GPQbot/src/main/java/event/ReactionEvent.java
+++ GPQbot/src/main/java/event/ReactionEvent.java
@@ -34,50 +34,48 @@ public class ReactionEvent extends ListenerAdapter {
 
             if (reactedMsg.getContentRaw().startsWith("Closing registration for ")) {
 
+                Optional permission = event.getMember().getRoles().stream().filter(x -> x.getName().equalsIgnoreCase(Settings.BOT_MOD_NAME)).findFirst();
+
+                if (!permission.isPresent())
+                    return;
+
                 if (Arrays.equals(event.getReactionEmote().getAsReactionCode().getBytes(StandardCharsets.UTF_8), check)) {
 
-                    Optional permission = event.getMember().getRoles().stream().filter(x -> x.getName().equalsIgnoreCase(Settings.BOT_MOD_NAME)).findFirst();
+                    event.getChannel().sendMessage("Confirmed GPQ at " + ZonedDateTime.now(ZoneId.of("GMT+8")).format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss (EEE)"))).queue();
 
-                    permission.ifPresent(
-                            x -> {
-                                event.getChannel().sendMessage("Confirmed GPQ at " + ZonedDateTime.now(ZoneId.of("GMT+8")).format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss (EEE)"))).queue();
+                    String creationMsgId = Data.currentGPQList.getByGuildKey(event.getGuild().getIdLong());
+                    creationMsgId = creationMsgId.substring(creationMsgId.lastIndexOf("/") + 1);
 
-                                String creationMsgId = Data.currentGPQList.getByGuildKey(event.getGuild().getIdLong());
-                                creationMsgId = creationMsgId.substring(creationMsgId.lastIndexOf("/") + 1);
+                    Message actualEb = event.getChannel().getHistoryAround(creationMsgId, 1).complete().getRetrievedHistory().get(0);
+                    if (actualEb.isPinned())
+                        actualEb.unpin().queue();
 
-                                Message actualEb = event.getChannel().getHistoryAround(creationMsgId, 1).complete().getRetrievedHistory().get(0);
-                                if (actualEb.isPinned())
-                                    actualEb.unpin().queue();
+                    Data.currentGPQList.remove(event.getGuild().getIdLong()); //once finalised, it will be removed from current.
 
-                                Data.currentGPQList.remove(event.getGuild().getIdLong()); //once finalised, it will be removed from current.
+                    List<MessageReaction> ebReact = actualEb.getReactions();
 
-                                List<MessageReaction> ebReact = actualEb.getReactions();
+                    int choice = ZonedDateTime.now(ZoneId.of("GMT+8")).getDayOfWeek().getValue() - 1;
+                    List<User> usrList = ebReact.get(choice).retrieveUsers().stream().collect(Collectors.toList());
 
-                                int choice = ZonedDateTime.now(ZoneId.of("GMT+8")).getDayOfWeek().getValue() - 1;
-                                List<User> usrList = ebReact.get(choice).retrieveUsers().stream().collect(Collectors.toList());
-
-                                String allName = "Participants (" + (usrList.size() - 1) + "): \n";
-                                for (User u : usrList) {
-                                    if (!u.getName().equals(event.getJDA().getSelfUser().getName())) {
-                                        UserAccount ua = Data.currentUserList.getByUserKey(u.getIdLong(), event.getGuild().getMember(u).getEffectiveName());
-                                        if (ua == null) {
-                                            allName += event.getGuild().getMember(u).getEffectiveName() + "\n";
-                                        } else {
-                                            if (ua.getFloor() > 0) {
-                                                allName += ua.getIgn() + "/" + ua.getFloor() + "\n";
-                                            } else {
-                                                allName += ua.getIgn() + "\n";
-                                            }
-                                        }
-                                    }
+                    String allName = "Participants (" + (usrList.size() - 1) + "): \n";
+                    for (User u : usrList) {
+                        if (!u.getName().equals(event.getJDA().getSelfUser().getName())) {
+                            UserAccount ua = Data.currentUserList.getByUserKey(u.getIdLong(), event.getGuild().getMember(u).getEffectiveName());
+                            if (ua == null) {
+                                allName += event.getGuild().getMember(u).getEffectiveName() + "\n";
+                            } else {
+                                if (ua.getFloor() > 0) {
+                                    allName += ua.getIgn() + "/" + ua.getFloor() + "\n";
+                                } else {
+                                    allName += ua.getIgn() + "\n";
                                 }
-                                event.getChannel().sendMessage(allName).queue();
-
-                                //TODO: send excel output
-                                //event.getUser().openPrivateChannel().flatMap(hi -> hi.sendMessage("Hello~")).queue();
                             }
-                    );
+                        }
+                    }
+                    event.getChannel().sendMessage(allName).queue();
 
+                    //TODO: send excel output
+                    //event.getUser().openPrivateChannel().flatMap(hi -> hi.sendMessage("Hello~")).queue();
                 }
 
                 reactedMsg.delete().queue();
