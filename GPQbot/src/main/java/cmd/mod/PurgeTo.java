5a95d569482b9d0949dcfba0fbeb6399c92c7d59
Tan Wei Jie <tanweijie123@gmail.com>
2021-05-06 13:57:39 +0800

    Change purgeto to use bulk delete for msg sent less than 2 weeks old

--- GPQbot/src/main/java/cmd/mod/PurgeTo.java
+++ GPQbot/src/main/java/cmd/mod/PurgeTo.java
@@ -6,8 +6,11 @@ import config.Settings;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.exceptions.ErrorResponseException;
 
+import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class PurgeTo extends Command {
 
@@ -24,13 +27,11 @@ public class PurgeTo extends Command {
             Message msgCommand = event.getMessage();
             List<Message> msgHistory = event.getChannel().getHistoryAfter(msgFrom, 100).complete().getRetrievedHistory(); //max limit = 100
 
-            String size = String.valueOf(msgHistory.size());
-
-            event.replyWarning(size + " pending delete. " +
-                    "Deletion will start in 5 seconds. If you had made a mistake, delete your `!nyan purgeto` command. ");
+            event.replyWarning(msgHistory.size() + " pending delete. " +
+                    "Deletion will start in 10 seconds. If you had made a mistake, delete your `!nyan purgeto` command. ");
 
             try {
-                TimeUnit.SECONDS.sleep(5);
+                TimeUnit.SECONDS.sleep(10);
                 event.getChannel().retrieveMessageById(msgCommand.getIdLong()).complete();
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -39,8 +40,20 @@ public class PurgeTo extends Command {
                 return;
             }
 
-            for (int i = 0; i < msgHistory.size(); i++) {
-                msgHistory.get(i).delete().queue();
+            //bulk delete msg < 2 weeks old, 2 <= size <= 100, manual delete otherwise.
+            OffsetDateTime twoWeeksOld = OffsetDateTime.now().minusWeeks(2);
+            Map<Boolean, List<Message>> resultMap = msgHistory.stream().collect(Collectors.partitioningBy(x -> x.getTimeCreated().isAfter(twoWeeksOld)));
+
+            if (resultMap.get(true).size() > 1) {
+                event.getTextChannel().deleteMessages(resultMap.get(true)).queue();
+            } else if (resultMap.get(true).size() == 1) {
+                resultMap.get(true).get(0).delete().queue();
+            }
+
+            List<Message> falseList = resultMap.get(false);
+
+            for (int i = 0; i < falseList.size(); i++) {
+                falseList.get(i).delete().queue();
             }
 
             msgFrom.delete().queue(x -> {
